3c5a19263946096f639daa36ac693cf43790dcd4
javdev20 <islam.qayumov07@gmail.com>
2023-01-23 19:43:57 +0100

    created togglebutton

---
+++ togglebutton/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import ToggleButton from "./ToggleButton";
+
+const App = () => {
+  return (
+    <div className="app">
+      <h3>Guidance Systems</h3>
+      <ToggleButton />
+    </div>
+  );
+};
+
+export default App;
+
